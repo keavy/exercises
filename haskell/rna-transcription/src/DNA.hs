e4babae8237691b2166d2b4c4d09d72558eb9afc
Keavy McMinn <keavy@users.noreply.github.com>
2016-12-07 10:41:20 -0700

    2nd pass: folding

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -1,17 +1,16 @@
 module DNA (toRNA) where
 
 toRNA :: String -> Maybe String
-toRNA text = case text of
-  [] -> Just ""
-  x:xs -> case (mapChar x, toRNA xs) of
-    (Just c, Just rest) -> Just (c:rest)
-    _ -> Nothing
-    where mapChar x = case x of
-            'G' -> Just 'C'
-            'C' -> Just 'G'
-            'T' -> Just 'A'
-            'A' -> Just 'U'
-            _ -> Nothing
+toRNA text = foldr combine (Just "") text
+  where combine :: Char -> Maybe String -> Maybe String
+        combine element rest = case (mapChar element, rest) of
+          (Just c, Just cs) -> Just (c:cs)
+          _ -> Nothing
+
+        foldr :: (Char -> Maybe String -> Maybe String) -> Maybe String -> [Char] -> Maybe String
+        foldr f seed list = case list of
+          [] -> seed
+          (head:tail) -> f head (foldr f seed tail)
 
 mapChar :: Char -> Maybe Char
 mapChar x = case x of
